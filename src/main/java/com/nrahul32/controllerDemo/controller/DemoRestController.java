c04c30a9dda7240c5b5f8085b17f0d9686c35d7c
Rahul N <rahul.eee32@gmail.com>
2023-03-02 19:40:18 +0530

    Multi URI mappings

--- src/main/java/com/nrahul32/controllerDemo/controller/DemoRestController.java
+++ src/main/java/com/nrahul32/controllerDemo/controller/DemoRestController.java
@@ -3,20 +3,24 @@ package com.nrahul32.controllerDemo.controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * localhost:8080/demo
- *
+ * localhost:8080/demo2
+ * localhost:8080/demo3
  */
 @RestController
-@RequestMapping("demo2")
+@RequestMapping(value = {"demo2", "demo3"})
 public class DemoRestController {
 
     /**
-     * localhost:8080/demo/hello
+     * localhost:8080/demo2/hello
+     * localhost:8080/demo2/greet
+     * localhost:8080/demo3/hello
+     * localhost:8080/demo3/greet
+     *
      * GET
      *
      * @ResponseBody is not needed since this uses @RestController
      */
-    @GetMapping("/hello")
+    @GetMapping(value = {"/hello", "/greet"})
     public String sayHello(){
         return "hello World! - from demo rest controller";
     }
