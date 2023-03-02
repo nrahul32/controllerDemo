b29b2c450619a68253a0b03d6bd08cbf4223f789
Rahul N <rahul.eee32@gmail.com>
2023-03-02 17:59:27 +0530

    Initial commit

---
+++ src/main/java/com/nrahul32/controllerDemo/ControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nrahul32.controllerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nrahul32/controllerDemo/ControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrahul32.controllerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
