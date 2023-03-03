b2728d80348598fd62b5c8319d7e11a61ea6b371
Rahul N <rahul.eee32@gmail.com>
2023-03-03 12:29:27 +0530

    Post operation to create an idea, Get to fetch all ideas

--- src/main/java/com/nrahul32/controllerDemo/controller/IdeaController.java
+++ src/main/java/com/nrahul32/controllerDemo/controller/IdeaController.java
@@ -2,17 +2,17 @@ package com.nrahul32.controllerDemo.controller;
 
 import com.nrahul32.controllerDemo.dto.IdeaDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = {"/demo_app"})
+@RequestMapping(value = {"/demo_app/v1"})
 public class IdeaController {
 
     /**
@@ -31,7 +31,7 @@ public class IdeaController {
     /**
      * Search an idea based on the id
      * GET
-     * localhost:8080/demo_app/ideas/1
+     * localhost:8080/demo_app/v1/ideas/1
      *
      * @param id
      * @return IdeaDTO
@@ -40,5 +40,52 @@ public class IdeaController {
     @GetMapping(value = {"/ideas/{id}"})
     public ResponseEntity getIdeaOfId(@PathVariable(name="id") int id) {
         return new ResponseEntity(IDEA_MAP.get(id), HttpStatus.OK);
+    }
+
+    /**
+     * CRUD
+     *
+     * Create - POST    - @PostMapping
+     * Read   - GET     - @GetMapping
+     * Update - UPDATE  - @PutMapping
+     * Delete - DELETE  - @DeleteMapping
+     *
+     */
+
+    /**
+     * Create an Idea
+     * POST
+     * localhost:8080/demo_app/v1/ideas
+     *
+     * BODY:
+     * {
+     *     "id":5,
+     *     "title":"new",
+     *     "author":"me",
+     *     "description":"new idea added via post"
+     * }
+     *
+     * RequestBody is needed to map the JSON passed in request to the IdeaDTO object
+     * Consumes indicates this method only accepts JSON in request body
+     */
+    @PostMapping(value = {"/ideas"}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createIdea(@RequestBody IdeaDTO ideaDTO) {
+        IDEA_MAP.put(ideaDTO.getId(), ideaDTO);
+        return new ResponseEntity(ideaDTO, HttpStatus.OK);
+    }
+
+    /**
+     * Gets all the ideas
+     *
+     * GET
+     * localhost:8080/demo_app/v1/ideas
+     */
+    @GetMapping(value = {"/ideas"})
+    public ResponseEntity fetchAllIdeas() {
+        List<IdeaDTO> ideas = new ArrayList<>();
+        for(IdeaDTO idea : IDEA_MAP.values()) {
+            ideas.add(idea);
+        }
+        return  new ResponseEntity(ideas, HttpStatus.OK);
     }
 }
